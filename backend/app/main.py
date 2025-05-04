7431ed97ce4e23105326cfc5eca9cfdc4688ae1b
E-Terra-institute <institute@e-terra.com.ua>
2025-05-04 19:44:44 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
+from app.api import patients
+from app.db import create_db
+
+app = FastAPI(title="EMR-lite API")
+
+app.include_router(patients.router, prefix="/patients")
+
+@app.on_event("startup")
+def startup():
+    create_db()
 
