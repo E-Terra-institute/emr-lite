d33129f5adab28bdac01e609966a9b9ac783aed3
E-Terra-institute <institute@e-terra.com.ua>
2025-05-04 19:46:30 +0300

    Update crud.py

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,1 +1,13 @@
+from sqlalchemy.orm import Session
+from app import models, schemas
+
+def get_patients(db: Session):
+    return db.query(models.Patient).all()
+
+def create_patient(db: Session, patient: schemas.PatientCreate):
+    db_patient = models.Patient(**patient.dict())
+    db.add(db_patient)
+    db.commit()
+    db.refresh(db_patient)
+    return db_patient
 
