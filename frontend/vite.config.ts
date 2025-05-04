1d2ec85175cf31fa80297d3faec7bafb11a55a93
E-Terra-institute <institute@e-terra.com.ua>
2025-05-04 19:49:08 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,1 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/patients': 'http://localhost:8000',
+    },
+  },
+})
 
