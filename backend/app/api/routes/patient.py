f9fd57d44aafd13c3cccf41e3e34e68e866a46d2
E-Terra-institute <institute@e-terra.com.ua>
2025-05-04 19:59:04 +0300

    Update patient.py

--- backend/app/api/routes/patient.py
+++ backend/app/api/routes/patient.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.db import SessionLocal
-from app import schemas, crud
+from app.db.session import SessionLocal
+from app.schemas.patient import PatientCreate, PatientOut
+from app.crud.patient import get_all, create
 
 router = APIRouter()
 
@@ -12,10 +13,10 @@ def get_db():
     finally:
         db.close()
 
-@router.get("/", response_model=list[schemas.PatientOut])
+@router.get("/", response_model=list[PatientOut])
 def list_patients(db: Session = Depends(get_db)):
-    return crud.get_patients(db)
+    return get_all(db)
 
-@router.post("/", response_model=schemas.PatientOut)
-def add_patient(patient: schemas.PatientCreate, db: Session = Depends(get_db)):
-    return crud.create_patient(db, patient)
+@router.post("/", response_model=PatientOut)
+def add_patient(patient: PatientCreate, db: Session = Depends(get_db)):
+    return create(db, patient)
