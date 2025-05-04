3a3bdf886051f77a8fc893a9a138bf429c3a5c39
E-Terra-institute <institute@e-terra.com.ua>
2025-05-04 19:58:07 +0300

    Update patient.py

--- backend/app/models/patient.py
+++ backend/app/models/patient.py
@@ -1,1 +1,12 @@
+from sqlalchemy import Column, Integer, String, Date
+from app.db.base import Base
+
+class Patient(Base):
+    __tablename__ = "patients"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, nullable=False)
+    date_of_birth = Column(Date, nullable=False)
+    phone = Column(String, nullable=False)
+    address = Column(String)
 
